13408c795645c99e71722a7ac9d661c38d6d78bc
Abe1able <abel.workmail@gmail.com>
2023-05-13 01:48:18 +0300

    Add current user to application controller and routes config

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,17 @@ Rails.application.routes.draw do
   #   resources :posts, only: [:index, :show]
   # end
 
-  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
-  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
-  get "/users/:id", to: "users#show", as: "user"
-  get "/users", to: "users#index"
+  # get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
+  # get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  # get "/users/:id", to: "users#show", as: "user"
+  # get "/users", to: "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 
   # Defines the root path route ("/")
    root "users#index"
